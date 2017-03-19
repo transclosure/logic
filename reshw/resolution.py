99a4e624d32e8a4f4f2937ab69a34a9a8b3b3d96
transclosure <natasha_danas@brown.edu>
2017-03-19 15:26:20 -0400

    basically done.

--- reshw/resolution.py
+++ reshw/resolution.py
@@ -1,15 +1,52 @@
 # Natasha Danas - Resolution Implementation
 from sys import stdin
 '''
+Constants
+'''
+def UNSAT(): return "UNSAT"
+def SAT(): return "SAT"
+'''
+Runs one resolution step
+'''
+def resstep(clauses, p):
+    pclauses = []
+    npclauses = []
+    oclauses = []
+    for clause in clauses:
+        if p in clause:  
+            clause.remove(p)   
+            pclauses.append(clause)
+        elif -p in clause:
+            clause.remove(-p)  
+            npclauses.append(clause)
+        else:               
+            oclauses.append(clause)
+    for pclause in pclauses:
+        for npclause in npclauses:
+            oclauses.append(pclause|npclause)
+    return oclauses
+'''
+Runs a resolution loop
+'''
+def resloop(clauses, variables):
+    for p in variables:
+        clauses = resstep(clauses, p)
+        if set() in clauses:
+            return UNSAT()
+    return SAT()
+'''
 Runs the main
 '''
 def main():
     clauses = []
-    for variables in stdin:
-        clause = []
-        for variable in variables.split():
-            clause.append(int(variable))
+    variables = set()
+    for line in stdin:
+        clause = set()
+        for word in line.split():
+            variable = int(word)
+            clause.add(variable)
+            variables.add(abs(variable))
         clauses.append(clause)
-    print clauses
+    print resloop(clauses, variables)
     return
 main()
