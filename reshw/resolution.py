4fddd4429896f5eb0ae489073e1e9d9002f14191
transclosure <natasha_danas@brown.edu>
2017-03-19 14:53:15 -0400

    dealing with i/o format

--- reshw/resolution.py
+++ reshw/resolution.py
@@ -1,9 +1,15 @@
 # Natasha Danas - Resolution Implementation
-from sys import argv
+from sys import stdin
 '''
 Runs the main
 '''
 def main():
-  print "hello"
-  return
+    clauses = []
+    for variables in stdin:
+        clause = []
+        for variable in variables.split():
+            clause.append(int(variable))
+        clauses.append(clause)
+    print clauses
+    return
 main()
