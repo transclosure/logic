626cc473eae491125f3949cc7a673e37c58d14d1
transclosure <natasha_danas@brown.edu>
2017-05-03 18:50:32 -0400

    hw1 done

---
+++ ocelot/alloy1.rkt
@@ -0,0 +1,37 @@
+#lang rosette
+(require ocelot)
+
+; Relations
+(define rR (-> univ univ))
+; Forumlas
+(define fNonempty
+  (some rR))
+(define fTransitive
+  (in (join rR rR) rR))
+(define fIrreflexive
+  (no (& iden rR)))
+(define fSymmetric
+  (in (~ rR) rR))
+(define fFunctional
+  (in (join (~ rR) rR) iden))
+(define fInjective
+  (in (join rR (~ rR)) iden))
+(define fTotal
+  (in univ (join rR univ)))
+(define fOnto
+  (in univ (join univ rR)))
+
+; Universes
+(define U (universe (build-list 4 values)))
+(define uR (universe-atoms U))
+; Bounds
+(define bR (make-product-bound rR uR uR))
+(define B (bounds U (list bR)))
+; Interpretations
+(define I (instantiate-bounds B))
+
+; Commands
+(define c1 (solve (assert (interpret* fNonempty I))))
+; Models
+(define m1 (interpretation->relations (evaluate I c1)))
+m1
