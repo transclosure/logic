4ec3cc7d7fc8b47d3d1bc627ea90b912669d9a71
transclosure <natasha_danas@brown.edu>
2017-05-04 13:27:39 -0400

    kittybacon one sig still causing problems

--- ocelot/kittybacon.rkt
+++ ocelot/kittybacon.rkt
@@ -6,6 +6,7 @@
 (define rKittyBacon (declare-relation 1 "one KittyBacon : extends Cat"))
 (define rFriends (declare-relation 2 "Cat.friends : Cat -> Cat"))
 ; Functions / Predicates / Asserts
+; TODO type system structs
 (define (F c)
   (join c rFriends))
 (define (FF c)
@@ -14,14 +15,15 @@
   (- (- (- (F (F (F c))) (F (F c))) (F c)) c))
 (define (CONNECTIONSOF c)
   (+ (+ (F c) (F (F c))) (F (F (F c)))))
-(define CONNECTED
+(define (CONNECTED)
   (= (- rCat rKittyBacon) (CONNECTIONSOF rKittyBacon)))
-(define SCONNECTED
+(define (SCONNECTED)
   (in (- rCat rKittyBacon) (join rKittyBacon (^ rFriends))))
-(define ISSUPERCONNECTED
-  (and (=> CONNECTED SCONNECTED)
-       (=> SCONNECTED CONNECTED)))
+(define (ISSUPERCONNECTED)
+  (and (=> (CONNECTED) (SCONNECTED))
+       (=> (SCONNECTED) (CONNECTED))))
 ; Facts
+; TODO type system macros
 (define fFriends
   (and (in (join rFriends univ) rCat)
        (in (join univ rFriends) rCat)))
@@ -38,28 +40,32 @@
   (= rFriends (~ rFriends)))
 
 ; Universes
-; TODO parameterize for quick bounds changes like in alloy
-(define uCat (build-list 3 (lambda (v) (gensym "Cat"))))
+; TODO type system w/ optimal bound semantics of 'one sig', 'extends', 'exactly'...
+(define uCat (build-list 5 (lambda (v) (gensym "Cat"))))
 (define uKittyBacon uCat)
 (define U (universe (append uCat uKittyBacon)))
 ; Bounds
-(define bCat (make-upper-bound rCat (map list uCat)))
+(define bCat (make-exact-bound rCat (map list uCat)))
 (define bKittyBacon (make-upper-bound rKittyBacon (map list uCat)))
 (define bFriends (make-product-bound rFriends uCat uCat))
-(define B (bounds U (list bCat bFriends bKittyBacon)))
+(define B (bounds U (list bCat bKittyBacon bFriends)))
 ; Interpretations
 (define I (instantiate-bounds B))
 
 ; Commands
-; TODO parameterize facts, assert, run etc
-(define c1 (verify (assert (interpret*
-                            (=> (and fFriends
-                                     fKittyBacon
-                                     fNoFriendlessCats
-                                     fNoSelfFriendship
-                                     fSymmetricFriendship)
-                                ISSUPERCONNECTED)
-                            I))))
+(define (RUN assume query verify/solve?)
+  (let* ([ocelot (=> assume query)]
+         [rosette (interpret* ocelot I)]
+         [cmd (assert rosette)]
+         [res (if verify/solve? (verify cmd) (solve cmd))])
+    (if (unsat? res) res (interpretation->relations (evaluate I res)))))
+(define (verifyISSUPERCONNECTED) (RUN (and fFriends
+                                           fKittyBacon
+                                           fNoFriendlessCats
+                                           fNoSelfFriendship
+                                           fSymmetricFriendship)
+                                      (ISSUPERCONNECTED)
+                                      #f))
 ; Models
-(define m1 (interpretation->relations (evaluate I c1)))
-m1
+; TODO pretty printer
+(verifyISSUPERCONNECTED)
