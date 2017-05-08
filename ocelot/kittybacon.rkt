2e318965fbc2112c21c1077f79add9d522a65510
transclosure <natasha_danas@brown.edu>
2017-05-07 20:50:41 -0400

    even I am running into issues with bounds and debugging..

--- ocelot/kittybacon.rkt
+++ ocelot/kittybacon.rkt
@@ -1,5 +1,6 @@
 #lang rosette
 (require ocelot)
+(current-bitwidth #f)
 
 ; Relations
 (define rCat (declare-relation 1 "Cat : Cat"))
@@ -14,11 +15,11 @@
 (define (FFF c)
   (- (- (- (F (F (F c))) (F (F c))) (F c)) c))
 (define (CONNECTIONSOF c)
-  (+ (+ (F c) (F (F c))) (F (F (F c)))))
+  (+ (+ (F c) (FF c)) (FFF c)))
 (define (CONNECTED)
   (= (- rCat rKittyBacon) (CONNECTIONSOF rKittyBacon)))
 (define (SCONNECTED)
-  (in (- rCat rKittyBacon) (join rKittyBacon (^ rFriends))))
+  (= (- rCat rKittyBacon) (join rKittyBacon (^ rFriends))))
 (define (ISSUPERCONNECTED)
   (and (=> (CONNECTED) (SCONNECTED))
        (=> (SCONNECTED) (CONNECTED))))
@@ -41,12 +42,12 @@
 
 ; Universes
 ; TODO type system w/ optimal bound semantics of 'one sig', 'extends', 'exactly'...
-(define uCat (build-list 5 (lambda (v) (gensym "Cat"))))
-(define uKittyBacon uCat)
+(define uCat (build-list 4 (lambda (v) (gensym "Cat"))))
+(define uKittyBacon (list (first uCat)))
 (define U (universe (append uCat uKittyBacon)))
 ; Bounds
 (define bCat (make-exact-bound rCat (map list uCat)))
-(define bKittyBacon (make-upper-bound rKittyBacon (map list uCat)))
+(define bKittyBacon (make-exact-bound rKittyBacon (map list uKittyBacon)))
 (define bFriends (make-product-bound rFriends uCat uCat))
 (define B (bounds U (list bCat bKittyBacon bFriends)))
 ; Interpretations
@@ -54,7 +55,7 @@
 
 ; Commands
 (define (RUN assume query verify/solve?)
-  (let* ([ocelot (=> assume query)]
+  (let* ([ocelot (if verify/solve? (=> assume query) (and assume query))]
          [rosette (interpret* ocelot I)]
          [cmd (assert rosette)]
          [res (if verify/solve? (verify cmd) (solve cmd))])
@@ -64,8 +65,9 @@
                                            fNoFriendlessCats
                                            fNoSelfFriendship
                                            fSymmetricFriendship)
-                                      (ISSUPERCONNECTED)
+                                      (not (ISSUPERCONNECTED))
                                       #f))
 ; Models
 ; TODO pretty printer
 (verifyISSUPERCONNECTED)
+
