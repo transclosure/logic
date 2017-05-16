1b3cbe04b632e2713204f71093158b926e4e781f
transclosure <natasha_danas@brown.edu>
2017-05-16 11:47:09 -0400

    it took me weeks, writing my own macros, and writing my own evaluator, to get enough useful ocelot output, to realize i used an '=' instead of an 'in'... we could run a study to compare how students do with/without my helper functions, but the immaturity of ocelot makes this a strawman user study. not worth our time. we need to talk to emina/ocelot-grad.

--- lab1.rkt
+++ lab1.rkt
@@ -3,7 +3,7 @@
 
 ; Universe of Discourse
 (require "DISCOURSE.rkt")
-(declare-sig #t 3 "Cat")
+(declare-sig #t 4 "Cat")
 (declare-sig #t 1 "KittyBacon" "Cat")
 (declare-rel "Cat" 2 "friends")
 ; Functions / Predicates / Asserts
@@ -16,9 +16,11 @@
 (define (CONNECTIONSOF c)
   (+ (+ (F c) (FF c)) (FFF c)))
 (define (CONNECTED)
-  (= (- (THIS "Cat") (THIS "KittyBacon")) (CONNECTIONSOF (THIS "KittyBacon"))))
+  (= (- (THIS "Cat") (THIS "KittyBacon"))
+     (CONNECTIONSOF (THIS "KittyBacon"))))
 (define (SCONNECTED)
-  (= (- (THIS "Cat") (THIS "KittyBacon")) (join (THIS "KittyBacon") (^ (THIS "friends")))))
+  (in (- (THIS "Cat") (THIS "KittyBacon"))
+     (join (THIS "KittyBacon") (^ (THIS "friends")))))
 (define (ISSUPERCONNECTED)
   (and (=> (CONNECTED) (SCONNECTED))
        (=> (SCONNECTED) (CONNECTED))))
@@ -32,10 +34,12 @@
 (define fSymmetricFriendship
   (= (THIS "friends") (~ (THIS "friends"))))
 ; Models
-(define (verifyISSUPERCONNECTED)
+(define (verifyISSUPERCONNECTED [query none])
   (declare-cmd (and fNoFriendlessCats
                     fNoSelfFriendship
                     fSymmetricFriendship
                     (not (ISSUPERCONNECTED)))
-               (join (THIS "KittyBacon") (THIS "friends"))))
-(verifyISSUPERCONNECTED)
+               query))
+(verifyISSUPERCONNECTED
+ (join (THIS "KittyBacon") (^ (THIS "friends")))
+ )
