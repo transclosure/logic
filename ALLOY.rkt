f0f91f92d04a0dbe161d1425a8f01dd9be9b83ed
transclosure <natasha_danas@brown.edu>
2017-05-17 20:20:27 -0400

    most of the alloy core in redex.

---
+++ ALLOY.rkt
@@ -0,0 +1,92 @@
+#lang racket
+(require redex/reduction-semantics)
+(require redex/pict)
+
+;; taken from http://alloy.mit.edu/alloy/documentation/alloy4-grammar.txt
+;; flagrant (essentially sugar) or pedantic (unhelpful disjunction) syntax is omitted
+;; some useful but highly expressive syntax left out for now
+(define-language Alloy
+  ;; specification ::= [module] open* paragraph*
+  (spc ::= (par ...))
+  ;; module ::= "module" name  [ "["  ["exactly"] name  ("," ["exactly"] num)*    "]" ]
+  ;TODO(mod ::= ("module" nm  ["[" ["exactly"] nm ("," ["exactly"] num)* "]"]))
+  ;; open ::= ["private"]  "open"  name  [ "[" ref,+ "]" ]  [ "as" name ]
+  ;TODO(opn ::= (["private"] "open" nm ["["ref"]"] ["as" nm]))
+  ;; paragraph ::= factDecl | assertDecl | funDecl | cmdDecl | enatDecl | sigDecl
+  (par ::= fact fun cmd sig)
+  ;; factDecl ::= "fact" [name] block
+  (fact ::= (fact nm blk))
+  ;; assertDecl ::= "assert" [name] block
+  ;TODO(asrt ::= ("assert" [nm] blk))
+  ;; funDecl ::= ["private"] "fun" [ref "."] name "(" decl,* ")" ":" expr block
+  ;; funDecl ::= ["private"] "fun" [ref "."] name "[" decl,* "]" ":" expr block
+  ;; funDecl ::= ["private"] "fun" [ref "."] name                ":" expr block
+  ;; funDecl ::= ["private"] "pred" [ref "."] name "(" decl,* ")" block
+  ;; funDecl ::= ["private"] "pred" [ref "."] name "[" decl,* "]" block
+  ;; funDecl ::= ["private"] "pred" [ref "."] name                block
+  (fun ::= (fun nm (dcl ...) expr blk))
+  ;; cmdDecl ::= [name ":"] ("run"|"check") (name|block) scope
+  (cmd ::= (cmd nm blk scp))
+  ;; scope ::= "for" number                   ["expect" (0|1)]
+  ;; scope ::= "for" number "but" typescope,+ ["expect" (0|1)]
+  ;; scope ::= "for"              typescope,+ ["expect" (0|1)]
+  ;; scope ::=                                ["expect" (0|1)]
+  (scp ::= [nat (tscp ...)])
+  ;; typescope ::= ["exactly"] number [name|"int"|"seq"]
+  (tscp ::= [exactly nat nm] [nat nm])
+  ;; enumDecl ::= "enum" name "{" name  ("," name)*  "}"
+  ;TODO
+  ;; sigDecl ::= sigQual* "sig" name,+ [sigExt] "{" decl,* "}" [block]
+  (sig ::= (sig (sigq ...) (nm ...) sigx (dcl ...)))
+  ;; sigQual ::= "abstract" | "lone" | "one" | "some" | "private"
+  (sigq ::= abstract lone one some)
+  ;; sigExt ::= "extends" ref
+  ;; sigExt ::= "in" ref ["+" ref]*
+  (sigx ::= [extends rf])
+  ;; expr ::= "let" letDecl,+ blockOrBar
+  ;;       | quant decl,+    blockOrBar
+  ;;       | unOp expr
+  ;;       | expr binOp   expr
+  ;;       | expr arrowOp expr
+  ;;       | expr ["!"|"not"] compareOp expr
+  ;;       | expr ("=>"|"implies") expr "else" expr
+  ;;       | expr "[" expr,* "]"
+  ;;       |     number
+  ;;       | "-" number
+  ;;       | "none"
+  ;;       | "iden"
+  ;;       | "univ"
+  ;;       | "Int"
+  ;;       | "seq/Int"
+  ;;       | "(" expr ")"
+  ;;       | ["@"] name
+  ;;       | block
+  ;;       | "{" decl,+ blockOrBar "}"
+  ;TODO
+  ;; decl ::= ["private"] ["disj"] name,+ ":" ["disj"] expr
+  (dcl ::= [(nm ...) expr])
+  ;; letDecl ::= name "=" expr
+  (let ::= [nm expr])
+  ;; quant ::= "all" | "no" | "some" | "lone" | "one" | "sum"
+  (qnt ::= all no some lone one)
+  ;; binOp ::= "||" | "or" | "&&" | "and" | "&" | "<=>" | "iff"
+  ;;        | "=>" | "implies" | "+" | "-" | "++" | "<:" | ":>" | "." | "<<" | ">>" | ">>>"
+  (bop ::= \| & => <=> + - \.)
+  ;; arrowOp ::= ["some"|"one"|"lone"|"set"] "->" ["some"|"one"|"lone"|"set"]
+  ;TODO
+  ;; compareOp ::= "=" | "in" | "<" | ">" | "=<" | ">="
+  (cop ::= = in < > =< >=)
+  ; unOp ::= "!" | "no" | "some" | "lone" | "one" | "set" | "seq" | "#" | "~" | "*" | "^"
+  (uop ::= ! no some lone one set \# ~ * ^)
+  ; block ::= "{" expr* "}"
+  ; blockOrBar ::= block
+  ; blockOrBar ::= "|" expr
+  (blk ::= (expr ...))
+  ; name ::= ("this" | ID) ["/" ID]*
+  (nm ::= string)
+  ; ref ::= name | "univ" | "Int" | "seq/Int"
+  (rf ::= nm univ)
+  )
+(render-language Alloy)
+
+
