882cc189afb2aa3b29a6bba9f703d7e5e8e3eb07
transclosure <natasha_danas@brown.edu>
2017-05-19 01:07:47 -0400

    cleaned up file.

--- ALLOY.rkt
+++ ALLOY.rkt
@@ -1,29 +1,32 @@
 #lang rosette
+#|||||||||||||||||||||||||||||||||||||||
+| Alloy Compiler for Ocelot/Rosette/Z3 |
+|||||||||||||||||||||||||||||||||||||||#
 ;; mandatory
 (require ocelot)
 (require redex/reduction-semantics)
 ;; optional
 (require redex/pict)
 (require redex/gui)
-
-;; taken from http://alloy.mit.edu/alloy/documentation/alloy4-grammar.txt
-;; flagrant (essentially sugar) and pedantic (unhelpful disjunction) syntax are omitted
-;; some useful but highly expressive syntax left out for now
-(define-language Alloy
+#|||||||||
+| Syntax |
+|||||||||#
+;; Adapted from http://alloy.mit.edu/alloy/documentation/alloy4-grammar.txt
+(define-language ALLOY
   (pSPEC ::= pALLOY pOCELOT)
-  (pOCELOT ::= string) ;TODO how to hook in ocelot syntax tree???
+  (pOCELOT ::= string) ;TODO hook ocelot syntax tree
   ;; specification ::= [module] open* paragraph*
   (pALLOY ::= (pPar ...+))
   ;; module ::= "module" name  [ "["  ["exactly"] name  ("," ["exactly"] num)*    "]" ]
-  ;TODO(mod ::= ("module" nm  ["[" ["exactly"] nm ("," ["exactly"] num)* "]"]))
+  ;TODO
   ;; open ::= ["private"]  "open"  name  [ "[" ref,+ "]" ]  [ "as" name ]
-  ;TODO(opn ::= (["private"] "open" nm ["["ref"]"] ["as" nm]))
+  ;TODO
   ;; paragraph ::= factDecl | assertDecl | funDecl | cmdDecl | enumDecl | sigDecl
   (pPar ::= pFact pFunc pCmd pSig)
   ;; factDecl ::= "fact" [name] block
   (pFact ::= (fact pName pBlock))
   ;; assertDecl ::= "assert" [name] block
-  ;TODO(asrt ::= ("assert" [nm] blk))
+  ;TODO
   ;; funDecl ::= ["private"] "fun" [ref "."] name "(" decl,* ")" ":" expr block
   ;; funDecl ::= ["private"] "fun" [ref "."] name "[" decl,* "]" ":" expr block
   ;; funDecl ::= ["private"] "fun" [ref "."] name                ":" expr block
@@ -49,7 +52,7 @@
   ;; sigExt ::= "extends" ref
   ;; sigExt ::= "in" ref ["+" ref]*
   (pSigX ::= (extends pRef))
-  ;; expr ::= 
+  ;; expr ::=
   (pExpr ::= string)
   ;(let (ltd ...) pBlock)    ;; | "let" letDecl,+ blockOrBar
   ;(pQuant (dcl ...) pBlock) ;; | quant decl,+    blockOrBar
@@ -94,56 +97,63 @@
   ; ref ::= name | "univ" | "Int" | "seq/Int"
   (pRef ::= pName univ)
   )
-(render-language Alloy)
-;TODO well-typed Alloy (define-judgements)
-
-;; Reductions (Surface ALLOY --R-> Core ALLOY --F-> OCELOT)
+(render-language ALLOY)
+#|||||||||
+| Typing |
+|||||||||#
+;TODO
+(define-judgment-form ALLOY
+  #:mode (typeof I I O)
+  #:contract (typeof a b c)
+  ((typeof d e f))
+  ((typeof g h i))
+  ;...
+  )
+#||||||||||||
+| Semantics |
+||||||||||||#
 (define R
   (reduction-relation
-   Alloy #:domain SPEC
-   (--> pALLOY (rALLOY pALLOY) isthisarbitrary)
+   ALLOY #:domain SPEC
+   (--> pALLOY (rALLOY pALLOY))
    ))
-
-;; (meta)Functions (Core ALLOY --F-> OCELOT)
-(define-metafunction Alloy
+(define-metafunction ALLOY
   rALLOY : any -> any
   ((rALLOY (pPar ...)) ((rPar pPar) ...)))
-(define-metafunction Alloy
+(define-metafunction ALLOY
   rPar : any -> any
   ((rPar pFact) "fact")
   ((rPar pFunc) "func")
   ((rPar pCmd) "cmd")
   ((rPar pSig) "sig"))
-
-;; test interactions
-(define test1 (lambda ()
-                (traces R (term (rALLOY
-                                 ((fact "foo" ("expr1"
-                                               "expr2"
-                                               "expr3"))
-                                  (fact "bar" ("exprA"))
-                                  (func "add1" (("x" "Int")
-                                                ("y" "Int"))
-                                        "expr1"
-                                        ())
-                                  ))))))
-(define test2 (lambda ()
-                (traces R (term (rALLOY
-                                 ((sig (abstract some) ("State") (extends univ)
-                                       ("expr1"
-                                        "expr2"
-                                        "expr3"))
-                                  (sig (one) ("StateA" "StateB" "StateC") (extends "State")
-                                       ())
-                                  ))))))
-(define test3 (lambda ()
-                (traces R (term (rALLOY
-                                 ((cmd "verifyTypeSoundness"
-                                       ("block1"
-                                        "block2"
-                                        "block3")
-                                       (for 7 ()))
-                                  (cmd "show" () (for 7 ((exactly 2 "Cat")
-                                                         (4 "Dog"))))
-                                  ))))))
-
+#|||||||||||
+| Examples |
+|||||||||||#
+(define (trace spec) (traces R spec))
+(define x1 (term (rALLOY
+                  ((fact "foo" ("expr1"
+                                "expr2"
+                                "expr3"))
+                   (fact "bar" ("exprA"))
+                   (func "add1" (("x" "Int")
+                                 ("y" "Int"))
+                         "expr1"
+                         ())
+                   ))))
+(define x2 (term (rALLOY
+                  ((sig (abstract some) ("State") (extends univ)
+                        ("expr1"
+                         "expr2"
+                         "expr3"))
+                   (sig (one) ("StateA" "StateB" "StateC") (extends "State")
+                        ())
+                   ))))
+(define x3 (term (rALLOY
+                  ((cmd "verifyTypeSoundness"
+                        ("block1"
+                         "block2"
+                         "block3")
+                        (for 7 ()))
+                   (cmd "show" () (for 7 ((exactly 2 "Cat")
+                                          (4 "Dog"))))
+                   ))))
