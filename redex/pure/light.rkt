cefbbedf5f4d875f961c38cc5ea5c587863617fe
Tim Nelson <tbnelson@gmail.com>
2017-08-01 21:06:54 -0400

    very naive property checking for light

--- redex/pure/light.rkt
+++ redex/pure/light.rkt
@@ -1,5 +1,11 @@
 ; Traffic light (classic)
 ; Two ways to model: explicit and implicit "turns"
+; Implicit: state only tracks light colors, and it is non-deterministic who goes from (red red).
+; Explicit: state tracks light colors /and/ whose turn it is to go next.
+;
+; Safety property: always some light is red.
+; Liveness property: neither direction is starved; each always eventually gets to go.
+; Expect that both pass safety but implicit fails liveness. 
 
 #lang racket
 (require redex)
@@ -14,7 +20,11 @@
 
 (define-term initial-state-implicit (red red))
 (define-term initial-state-explicit (red red ns))
-(redex-match? lights intersection (term initial-state-explicit))
+; Test: terms work as expected?
+(redex-match? lights intersection-explicit (term initial-state-explicit))
+(redex-match? lights intersection-implicit (term initial-state-implicit))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; Implicit turns
 (define transition-implicit
@@ -38,8 +48,7 @@
         g1)     
    (--> (red red) 
         (red green)
-        g2)     
-  ))
+        g2)))
 
 ; Explicit turns
 (define transition-explicit
@@ -63,8 +72,11 @@
         g_ns)     
    (--> (red   red ew) 
         (green red ns)
-        g_ew)     
-  ))
+        g_ew)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Explore reachable states naively (more sanity checking of the "spec")
 
 (printf "Implicit states starting from~n")
 (term initial-state-implicit)
@@ -76,10 +88,7 @@ e2
 e3
 (define e4 (apply append (map (curry apply-reduction-relation transition-implicit) e3)))
 e4
-(traces transition-implicit (term initial-state-implicit))
-
-; QUESTION: why 2 (red red) states? Are they not considered equal?
-;    The cycle gets found in traces and stepper calls below...
+; Repeats are an artifact of search.
 
 ;(traces transition-implicit (term initial-state-implicit))
 ;(stepper transition-implicit (term initial-state-implicit))
@@ -98,4 +107,89 @@ e4exp
 e5exp
 (define e6exp (apply append (map (curry apply-reduction-relation transition-explicit) e5exp)))
 e6exp
+(define e7exp (apply append (map (curry apply-reduction-relation transition-explicit) e6exp)))
+e7exp
 ;(traces transition-explicit (term initial-state-explicit))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Property verification!
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Safety: always some red light
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; test-->> isn't quite what we want, because it implicitly calls apply-reduction-relation* w/o "all?" set.
+; Without that option it'l only return terms that don't reduce. Instead:
+
+(define reachable-explicit (apply-reduction-relation* #:all? #t #:cache-all? #t transition-explicit (term initial-state-explicit)))
+(define reachable-implicit (apply-reduction-relation* #:all? #t #:cache-all? #t transition-implicit (term initial-state-implicit)))
+; ^ note that this is only checking for reachability from START. 
+
+; Check safety: every reachable state has >=1 light red:
+(define (safety reachable)
+  (andmap (lambda (t) (or (symbol=? (first t) 'red)
+                          (symbol=? (second t) 'red))) reachable-explicit))
+
+(printf "Implicit safety (true means pass): ~a~n" (safety reachable-implicit))
+(printf "Explicit safety (true means pass): ~a~n" (safety reachable-explicit))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Liveness: non-starvation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; For each reachable state, does AFgreen hold?
+; (Inefficient search using apply-reduction-relation, not a CTL model checker!)
+
+
+; afphi returns #f if there is no counterexample starting from this state. 
+; otherwise: returns a path that falsifies the property
+(define (afphi visited phi? tr s) 
+  (define nextss (apply-reduction-relation tr s))         
+  (cond
+    ; If this state satisfies phi, suffices (slow positioning, could move up)
+    [(phi? s) #f]
+    ; No next states but does not satisfy phi
+    [(empty? nextss) visited]           
+    [else
+     ; If a child state meets phi, no need to check it.
+     ; If we find a cycle of unmet-phi states, fail.
+     (define obligations (filter (lambda (n) (not (phi? n))) nextss))
+     (cond [(check-duplicates (append obligations visited))
+            visited]
+           [else
+            (ormap (curry afphi (cons s visited) phi? tr) obligations)])]))
+
+(define (liveness-helper curried-afphi reachable)
+  (ormap (lambda (s)
+           (define result (curried-afphi s))
+           ; Here is where we'd add the path to s if we had it.
+           (cond [(list? result) result]
+                 [else #f])) reachable))
+
+; Check east-west liveness. Handwaving north-south via symmetry.
+(define liveness-implicit
+  (liveness-helper
+   (curry afphi
+          empty
+          ; A state satisfies phi if the east-west light is green
+          (lambda (s) (equal? s '(red green)))
+          transition-implicit)
+   reachable-implicit))
+
+; Check east-west liveness. Handwaving north-south via symmetry.
+(define liveness-explicit
+  (liveness-helper
+   (curry afphi
+          empty
+          ; A state satisfies phi if the east-west light is green
+          (lambda (s) (or (equal? s '(red green ew))))
+          transition-explicit)
+   reachable-explicit))
+
+; Observation: it's a bit tougher to sanity check the above than it is in Alloy,
+; since I'm not just writing a spec---I'm writing a very dumb model checker!
+
+(printf "Implicit liveness (false means pass): ~a~n" liveness-implicit)
+(printf "Explicit liveness (false means pass): ~a~n" liveness-explicit)
