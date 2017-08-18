117449831b5a70b4cff8d01bc81063207095aa8d
transclosure <natasha_danas@brown.edu>
2017-08-18 15:45:22 -0400

    redex does Y combination / recursion well. rosette does X combination well (see file for example). all we need to do is extend L with an SMT compatible closure to handle these new disjunctive choice subterms.

--- redex/pure/peg-solitaire.rkt
+++ redex/pure/peg-solitaire.rkt
@@ -1,4 +1,4 @@
-#lang rosette
+#lang rosette/safe
 
 (require redex)
 (require rosette/query/debug
@@ -13,18 +13,6 @@
 (define-language peg-solitaire
   (pos ::= █ ○ ●) 
   (board ::= ((pos ...) ...)))
-
-;; redex doesnt have a function for gettings the terminals of a language?!
-;; !!!there has to be a more direct term to bitvector approach based on the grammar ordering
-(define terminals
-  (list '█ '○ '●))
-(define terminalw
-  (+ 1 (integer-sqrt (length terminals))))
-(define (terminal->bitvector t)
-  (integer->bitvector (index-of terminals t) (bitvector terminalw)))
-(define (bitvector->terminal b)
-  (define tail (list-tail terminals (bitvector->natural b)))
-  (and (not (empty? tail)) (first tail)))
 
 #||||||||
 | Terms |
@@ -54,6 +42,15 @@
    (█ █ ● ● ● █ █)
    (█ █ ● ● ● █ █)))
 
+(define-term oneplaytowin-board
+  ((█ █ ○ ○ ○ █ █)
+   (█ █ ○ ○ ○ █ █)
+   (○ ○ ○ ○ ○ ○ ○)
+   (○ ○ ○ ○ ○ ○ ○)
+   (○ ○ ○ ○ ● ● ○)
+   (█ █ ○ ○ ○ █ █)
+   (█ █ ○ ○ ○ █ █)))
+
 (define-term lost-board
   ((█ █ ○ ○ ○ █ █)
    (█ █ ○ ○ ○ █ █)
@@ -71,23 +68,6 @@
    (○ ○ ○ ○ ○ ● ○)
    (█ █ ○ ○ ○ █ █)
    (█ █ ○ ○ ○ █ █)))
-
-;; should be a single bitvector, no?
-;; need a way to generalize kleene* to bitvectors without losing list structure
-;; or using hardcoded constants for a 7x7 board?
-(define (board->bitvector p**)
-  (apply concat (map terminal->bitvector (flatten p**))))
-(define (b->b* b i)
-  (define j (+ (- i terminalw) 1))
-  (cond ((< j 0) empty)
-        (else (cons (extract i j b) (b->b* b (- j 1))))))
-(define (b*->p* b*)
-  (map bitvector->terminal b*))
-(define (p*->p** p* p**)
-  (cond ((empty? p*) p**)
-        (else (p*->p** (drop p* 7) (cons (reverse (take p* 7)) p**)))))
-(define (bitvector->board b)
-  (p*->p** (b*->p* (reverse (b->b* b (- (* (* 7 7) terminalw) 1)))) empty))
 
 #|||||||||||||
 | Reductions |
@@ -154,116 +134,41 @@
 | Term Properties |
 ||||||||||||||||||#
 
-;; every core is the same length for this winning? (only the peg count changes)
-;; every move makes the same progress for board clearing...
-;; ... meaning even the structure of the core doesn't help us
-;; ways forward: strengthen phi? multiple phis? different smt query?
 (define/debug (winning? board)
   (define/debug pegs-left-on-board
     (count (curry equal? '●) (flatten board)))
   (= pegs-left-on-board 1))
 
-;; aha! we want to maximize being unstuck (ability to progress to satisfy winning?)
-;; lots of ways to define unstuck, staying fairly propositionalized (more syntax more effect)
-;; this is just defining winning as not losing, right?
-(define (match-peg/neighbors* board)
-  (redex-match peg-solitaire
-               (side-condition (any_1
-                                ...
-                                (pos_1 ...            pos_2            pos_3 ...)
-                                (pos_4 ... pos_5 pos_0 pos_6 pos_7 ...)
-                                (pos_8 ...            pos_9            pos_10 ...)
-                                any_2
-                                ...)
-                               (and (= (length (term (pos_1 ...)))
-                                       (+ (length (term (pos_4 ...))) 1)
-                                       (length (term (pos_8 ...))))
-                                    (= (length (term (pos_3 ...)))
-                                       (+ 1 (length (term (pos_7 ...))))
-                                       (length (term (pos_10 ...))))))
-               board))
-(define (peg/neighbors* board)
-  (define matches (match-peg/neighbors* board))
-  (map peg/neighbors matches))
-(define (peg/neighbors match)
-  (define bindings (match-bindings match))
-  (define peg (bind-exp (findf (lambda (abind) (equal? 'pos_0 (bind-name abind))) bindings)))
-  (define top (bind-exp (findf (lambda (abind) (equal? 'pos_2 (bind-name abind))) bindings)))
-  (define left (bind-exp (findf (lambda (abind) (equal? 'pos_5 (bind-name abind))) bindings)))
-  (define right (bind-exp (findf (lambda (abind) (equal? 'pos_6 (bind-name abind))) bindings)))
-  (define bot (bind-exp (findf (lambda (abind) (equal? 'pos_9 (bind-name abind))) bindings)))
-  (list peg top left right bot))
-
-(define/debug (stuck? p/ns)
-  (and
-   (equal? (first p/ns) '●)
-   (or (equal? (second p/ns) '○) (equal? (second p/ns) '█))
-   (or (equal? (third p/ns) '○) (equal? (third p/ns) '█))
-   (or (equal? (fourth p/ns) '○) (equal? (fourth p/ns) '█))
-   (or (equal? (fifth p/ns) '○) (equal? (fifth p/ns) '█))))
-(define/debug (lost? p/ns*)
-  (ormap stuck? p/ns*))
-
-;; synthesize a rewrite of the term such that phi no longer fails
-;; minimize the edit distance between the boards (xor is close but order biased)
-(define (rewrite-bitvector b phi?)
-  (define-symbolic bprime (bitvector (* (* 7 7) terminalw)))
-  (define sol (optimize #:minimize (list (bvxor b bprime))
-                        #:guarantee (assert (phi? (bitvector->board bprime)))))
-  (evaluate bprime sol))
-(define (write-board phi?)
-  (define newboard (a7x7board 1))
-  (define sol (optimize #:minimize empty
-                        #:guarantee (assert (phi? newboard))))
-  (evaluate newboard sol))
-
-(apply printf (cons "~n~a~n~a~n~a~n~a~n~a~n~a~n~a~n"
-                    (bitvector->board (rewrite-bitvector (board->bitvector (term lost-board))
-                                                         winning?))))
-(apply printf (cons "~n~a~n~a~n~a~n~a~n~a~n~a~n~a~n"
-                    (bitvector->board (rewrite-bitvector (board->bitvector (term won-board))
-                                                         (lambda (b) (not (winning? b)))))))
-(apply printf (cons "~n~a~n~a~n~a~n~a~n~a~n~a~n~a~n"
-                    (write-board winning?)))
-(apply printf (cons "~n~a~n~a~n~a~n~a~n~a~n~a~n~a~n"
-                    (write-board (lambda (b) (not (winning? b))))))
-
 #||||||||||||||||||||||||||
 | Term Reduction Checking |
 ||||||||||||||||||||||||||#
-;; all of this is currently obsolete!!!
 
-(define (core phi board)
-  (debug (boolean? integer?) (assert (phi (peg/neighbors* board)))))
-(define (core-weight phi board)
-  (with-handlers ((exn:fail? (lambda (e) -1)))
-    (string-length (~a (core phi board)))))
-  
-(define (search-for-solution board avoiding?)
-  (define (step board)
-    (apply printf (cons "~n~a~n~a~n~a~n~a~n~a~n~a~n~a~n" board))
-    (define (expand boards)
-      (append* (map (lambda (b) (begin
-                                  (define choices (apply-reduction-relation move b))
-                                  (if (empty? choices) b choices)))
-                    boards)))
-    (define next-boards
-      (expand (list board)))
-    (define next-weight/boards
-      (filter (lambda (w/b) (positive? (first w/b)))
-              (map (lambda (b) (cons (core-weight avoiding? b) b))
-                   next-boards)))
-    (cond ((empty? next-weight/boards)
-           (and (winning? board) (list board)))
-          (else
-           (define sorted-next-boards
-             (map rest (sort next-weight/boards
-                             (lambda (a b) (> (first a) (first b))))))
-           (define (dfs boards)
-             (cond ((empty? boards) false)
-                   (else
-                    (define next (step (first boards)))
-                    (cond ((false? next) (dfs (rest boards)))
-                          (else (cons board next))))))
-           (dfs sorted-next-boards))))
-  (step board))
+(define (find-rewrite oldboard phi?)
+  (define newboard (a7x7board 1))
+  (define sol (synthesize #:forall (list )
+                          #:guarantee (assert
+                                       (and (equal? (YXcombinator oldboard) newboard)
+                                            (phi? newboard)))))
+  (and (not (unsat? sol)) (evaluate newboard sol)))
+
+(define (YXcombinator board)
+  ;; for some reason choose cannot be used in context of racket/apply (unfolding)
+  (define (chooseboard boards)
+    (cond ((equal? (length boards) 1) (first boards))
+          (else (choose (first boards) (chooseboard (rest boards))))))
+  (chooseboard (apply-reduction-relation move board)))
+
+;; finding a single step rewrite is easy!
+(apply printf (cons "~n~a~n~a~n~a~n~a~n~a~n~a~n~a~n"
+                    (find-rewrite (term oneplaytowin-board) winning?)))
+;; redex is good at recursion down the tree (Ycombinator)
+;; rosette is good at disjunction across the tree (Xcombinator)
+;; Xcombinator is what I'm calling Emina's SVM that squishes (choose terms) into a single term
+;; example of it here
+(YXcombinator (term initial-board))
+;; but oh no! we can't do multi-step Y reduction because X combined terms aren't in the language!
+(YXcombinator (YXcombinator (term initial-board)))
+;; well... they're just subterms, and if my limited knowledge of PL is correct...
+;; all we have to do is extend the language with a SMT compatible closure to match/reduce these terms
+;; this may be more difficult in general, but for now, I can just extend peg-solitaire with the few
+;; disjunctive cases
