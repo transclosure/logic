f300b2588c500fc852ef50de4fe10f95eb993676
transclosure <natasha_danas@brown.edu>
2017-08-23 21:46:13 -0400

    fixed naive handling of symbolic unions. getting sound results for the 3 test cases in peg-solitaire. still hit a bottleneck at depth ~8. should compare to regular redex and see if it's any better / worse.

--- redex/pure/peg-solitaire.rkt
+++ redex/pure/peg-solitaire.rkt
@@ -25,15 +25,27 @@
   (cond ((union? p) (map (lambda (g.v) (list (car g.v) (cdr g.v)))
                          (union-contents p)))
         (else (list (list #t p)))))
+(define (unlift/board p**)
+  (map (lambda (p*) (map unlift/pos p*)) p**))
+
 (define (lift/pos p/su)
-  (cond ((<= (length p/su) 1) (second (first p/su)))
+  (cond ((<= (length p/su) 1) (first p/su))
         (else (apply union
                      (map (lambda (g.v) (cons (first g.v) (second g.v)))
                           p/su)))))
-(define (unlift/board p**)
-  (map (lambda (p*) (map unlift/pos p*)) p**))
 (define (lift/board p**/su)
-  (map (lambda (p*/su) (map lift/pos p*/su)) p**/su))
+  (define board (map (lambda (p*/su) (map lift/pos p*/su)) p**/su))
+  (define (and-l values)
+    (cond ((empty? values) #t)
+          (else (and (first values) (and-l (rest values))))))
+  (define guard (and-l (flatten
+                        (map (lambda (p*)
+                               (map (lambda (p)
+                                      (or (union? p) (first p))) p*)) board))))
+  (define value (map (lambda (p*)
+                       (map (lambda (p) (cond ((union? p) p)
+                                              (else (second p)))) p*)) board))
+  (list guard value))
 
 #||||||||
 | Terms |
@@ -158,71 +170,75 @@
   (reduction-relation
    peg-solitaire/su
    #:domain board/su
-   (--> (any_1
-         ...
-         (any_2 ...
-          (any_11 ... (any_101 ●) any_12 ...)
-          (any_13 ... (any_102 ●) any_14 ...)
-          (any_15 ... (any_103 ○) any_16 ...)
-          any_3 ...)
-         any_4
-         ...)
+   (--> (side-condition (any_1
+                         ...
+                         (any_2 ...
+                          (any_11 ... (any_101 ●) any_12 ...)
+                          (any_13 ... (any_102 ●) any_14 ...)
+                          (any_15 ... (any_103 ○) any_16 ...)
+                          any_3 ...)
+                         any_4
+                         ...)
+                        (and (term any_101) (term any_102) (term any_103)))
         (any_1
          ...
          (any_2 ...
-          (any_11 ... (any_101 ○) any_12 ...)
-          (any_13 ... (any_102 ○) any_14 ...)
-          (any_15 ... (any_103 ●) any_16 ...)
+          (((and any_101 any_102 any_103) ○))
+          (((and any_101 any_102 any_103) ○))
+          (((and any_101 any_102 any_103) ●))
           any_3 ...)
          any_4
          ...)
         →)
-   (--> (any_1
-         ...
-         (any_2 ...
-          (any_11 ... (any_101 ○) any_12 ...)
-          (any_13 ... (any_102 ●) any_14 ...)
-          (any_15 ... (any_103 ●) any_16 ...)
-          any_3 ...)
-         any_4
-         ...)
+   (--> (side-condition (any_1
+                         ...
+                         (any_2 ...
+                          (any_11 ... (any_101 ○) any_12 ...)
+                          (any_13 ... (any_102 ●) any_14 ...)
+                          (any_15 ... (any_103 ●) any_16 ...)
+                          any_3 ...)
+                         any_4
+                         ...)
+                        (and (term any_101) (term any_102) (term any_103))) 
         (any_1
          ...
          (any_2 ...
-          (any_11 ... (any_101 ●) any_12 ...)
-          (any_13 ... (any_102 ○) any_14 ...)
-          (any_15 ... (any_103 ○) any_16 ...)
+          (((and any_101 any_102 any_103) ●))
+          (((and any_101 any_102 any_103) ○))
+          (((and any_101 any_102 any_103) ○))
           any_3 ...)
          any_4
          ...)
         ←)
-   (--> (any_1
-         ...
-         (any_2 ..._1 (any_11 ... (any_101 ●) any_12 ...) any_3 ...)
-         (any_4 ..._1 (any_13 ... (any_102 ●) any_14 ...) any_5 ...)
-         (any_6 ..._1 (any_15 ... (any_103 ○) any_16 ...) any_7 ...)
-         any_8
-         ...)
+   (--> (side-condition (any_1
+                         ...
+                         (any_2 ..._1 (any_11 ... (any_101 ●) any_12 ...) any_3 ...)
+                         (any_4 ..._1 (any_13 ... (any_102 ●) any_14 ...) any_5 ...)
+                         (any_6 ..._1 (any_15 ... (any_103 ○) any_16 ...) any_7 ...)
+                         any_8
+                         ...)
+                        (and (term any_101) (term any_102) (term any_103)))
         (any_1
          ...
-         (any_2 ... (any_11 ... (any_101 ○) any_12 ...) any_3 ...)
-         (any_4 ... (any_13 ... (any_102 ○) any_14 ...) any_5 ...)
-         (any_6 ... (any_15 ... (any_103 ●) any_16 ...) any_7 ...)
+         (any_2 ... (((and any_101 any_102 any_103) ○)) any_3 ...)
+         (any_4 ... (((and any_101 any_102 any_103) ○)) any_5 ...)
+         (any_6 ... (((and any_101 any_102 any_103) ●)) any_7 ...)
          any_8
          ...)
         ↓)
-   (--> (any_1
-         ...
-         (any_2 ..._1 (any_11 ... (any_101 ○) any_12 ...) any_3 ...)
-         (any_4 ..._1 (any_13 ... (any_102 ●) any_14 ...) any_5 ...)
-         (any_6 ..._1 (any_15 ... (any_103 ●) any_16 ...) any_7 ...)
-         any_8
-         ...)
+   (--> (side-condition (any_1
+                         ...
+                         (any_2 ..._1 (any_11 ... (any_101 ○) any_12 ...) any_3 ...)
+                         (any_4 ..._1 (any_13 ... (any_102 ●) any_14 ...) any_5 ...)
+                         (any_6 ..._1 (any_15 ... (any_103 ●) any_16 ...) any_7 ...)
+                         any_8
+                         ...)
+                        (and (term any_101) (term any_102) (term any_103)))
         (any_1
          ...
-         (any_2 ... (any_11 ... (any_101 ●) any_12 ...) any_3 ...)
-         (any_4 ... (any_13 ... (any_102 ○) any_14 ...) any_5 ...)
-         (any_6 ... (any_15 ... (any_103 ○) any_16 ...) any_7 ...)
+         (any_2 ... (((and any_101 any_102 any_103) ●)) any_3 ...)
+         (any_4 ... (((and any_101 any_102 any_103) ○)) any_5 ...)
+         (any_6 ... (((and any_101 any_102 any_103) ○)) any_7 ...)
          any_8
          ...)
         ↑)
@@ -242,18 +258,19 @@
 ||||||||||||||||||||||||||#
 
 (define (Ycombinator board)
-  (apply-reduction-relation move/su board))
+  (apply-reduction-relation move/su (unlift/board board)))
 
 (define (Xcombinator boards)
+  (define board (lift/board (first boards)))
+  (define guard (first board))
+  (define value (second board))
   (cond ((> (length boards) 1)
-         ;; instead of making the guard an arbitrary choose...
-         ;; can we make it a function representing the reduction choice made
          (define-symbolic* b boolean?)
-         (if b (first boards) (Xcombinator (rest boards))))
-        (else (first boards))))
+         (if (and b guard) value (Xcombinator (rest boards))))
+        (else value)))
 
 (define (YXcombinator board)
-  (Xcombinator (map lift/board (Ycombinator (unlift/board board)))))
+  (Xcombinator (Ycombinator board)))
 
 (define (solve/su board phi?)
   (define sol (synthesize #:forall (list )
@@ -261,16 +278,16 @@
   (and (not (unsat? sol)) (evaluate board sol)))
 
 (define (search/su board phi?)
+  (apply printf "~a~n~a~n~a~n~a~n~a~n~a~n~a~n" board)
   (define found? (solve/su board phi?))
   (cond (found? (apply printf "~a~n~a~n~a~n~a~n~a~n~a~n~a~n~n~n" found?))
         (else
-         (printf "~a~n" found?)
          (search/su (YXcombinator board) phi?))))
 
-(search/su (term oneplaytowin-board) winning?)
-(search/su (term easywin-board) winning?)
+;(search/su (term oneplaytowin-board) winning?)
+;(search/su (term easywin-board) winning?)
 (search/su (term mediumwin-board) winning?)
 ;; move/su currently does not check guard values in match, which allows unsound reduction
 ;; unsound reductions are bloating search space, but rosette avoids unsound results when solving
 ;; can't solve the following because it takes too long (search space, symbolic union size, search alg)
-;;(search/su (term initial-board) winning?)
+;(search/su (term initial-board) winning?)
