1bfd8d595029e1785f1fba70e56915464ed92a47
transclosure <natasha_danas@brown.edu>
2017-09-14 16:06:35 -0400

    taking an honest approach to checking my language for untyped soundness (find me some expression that does not reduce and is not already a value) rewards me many annoying counterexamples from redex-check. im also very concerned with overconstraint here (predicating true counterexamples as annoying, agh). this may be even lower hanging fruit than hard to find counterexamples... unless there's a better verification approach than i'm taking with redex.

--- redex/lambdajs/jscore-test.rkt
+++ redex/lambdajs/jscore-test.rkt
@@ -21,3 +21,41 @@
 (define (success) (test (label x (break y (break x "success"))) "success"))
 (define (failure) (broken-test (label x (break y (break x "success"))) "success"))
 
+;; Without E-Break-Break, some term *should* get stuck (if we can find it)
+;; Best redex functionality available: (redex-check template property-expr kw-arg ...)
+;; optional kw-args may be helpful/harmful to redex search
+;; template pattern for generating terms may be helpful/harmful to redex search
+
+(define (value? t) (redex-match? broken-lambdaJS val t))
+;; redex quickly finds missing Err-Break-Reduction/Err-Break-No-Label rule, using fixed error-eval
+(define (reduces? t) (not (empty? (apply-reduction-relation eval-lambdaJS-errors t))))
+;; had to start adding catches for "annoying" counterexamples
+;; overconstraint is a big problem here!
+(define (annoying? t) (or (redex-match? broken-lambdaJS variable-not-otherwise-mentioned t)
+                          (redex-match? broken-lambdaJS (set! any_1 any_2) t)
+                          (redex-match? broken-lambdaJS (alloc any_1) t)
+                          (redex-match? broken-lambdaJS (deref any_1) t)
+                          (redex-match? broken-lambdaJS (get-field any_1 any_2) t)
+                          (redex-match? broken-lambdaJS (update-field any_1 any_2 any_3) t)
+                          (redex-match? broken-lambdaJS (delete-field any_1 any_2) t)
+                          (redex-match? broken-lambdaJS (throw any_1) t)
+                          (redex-match? broken-lambdaJS (label variable-not-otherwise-mentioned_1
+                                                               variable-not-otherwise-mentioned_2) t)
+                          (redex-match? broken-lambdaJS (break variable-not-otherwise-mentioned_1
+                                                               variable-not-otherwise-mentioned_2) t)
+                          (redex-match? broken-lambdaJS (variable-not-otherwise-mentioned) t)
+                          (redex-match? broken-lambdaJS (begin variable-not-otherwise-mentioned
+                                                               any ...) t)
+                          (redex-match? broken-lambdaJS (try-catch variable-not-otherwise-mentioned
+                                                                   e) t)
+                          (redex-match? broken-lambdaJS (try-finally variable-not-otherwise-mentioned
+                                                                   e) t)
+                          (redex-match? broken-lambdaJS (object (any_1
+                                                                 any_2)) t)
+                          (redex-match? broken-lambdaJS (variable-not-otherwise-mentioned val) t)
+                          ))
+
+(redex-check broken-lambdaJS e (or (value? (term e))
+                                   (reduces? (term (() e)))
+                                   (annoying? (term e)))
+             #:prepare (lambda (t) (begin (printf "checking: ~a~n" t) t)))
